adf474799e1c0213c47ab40a71e5e17446a2040b
failattu <kalle.sukeltaja@gmail.com>
2014-12-18 13:40:23 +0100

    Ubuntu tested scripts, under work

---
+++ debian/vertica.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+sudo su
+sudo sed -i 's/^exit 0 /#exit 0/' /etc/rc.local
+sudo /sbin/blockdev --setra 2048 /dev/sda
+sudo echo '/sbin/blockdev --setra 2048 /dev/sda' >> /etc/rc.local
+#sudo sed -i 's/^SELINUX=enabled/SELINUX=disabled/' /selinux/config
+sudo echo deadline > /sys/block/sda/queue/scheduler
+sudo echo 'echo deadline > /sys/block/sda/queue/scheduler' >> /etc/rc.local
+sudo apt-get install -y pstack mcelog sysstat
+sudo echo 'exit 0' >> /etc/rc.local
